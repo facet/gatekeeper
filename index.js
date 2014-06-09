d7dca9675f1832dc8348b994398498d6b17f6d26
Spoolphiz <alexander.manea@gmail.com>
2014-06-08 19:50:27 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var UsersAPI = require('ecapi.users')
+	, util = require('util')
+  , EventEmitter = require('events').EventEmitter;
+  // , ResponseHandler = require('response-handler');
+
+var GateKeeper = function( options ){
+	this.options = options;
+	this.registerEvents();
+}
+
+// inherit the prototype methods from EventEmitter
+util.inherits(GateKeeper, EventEmitter);
+
+GateKeeper.prototype.registerEvents = function() {
+	// var _this = this;
+
+	/*
+	* On data from user module, trigger response
+	*/
+	this.on('User:data', function( req, res, next, data ) {
+		if( data.hasOwnProperty('id') ) {
+			req.user = data;
+			next();
+		}
+		else {
+			// TODO: replace this with something like event based response handler, ie:
+			// this.emit('response:error', req, res, next, {message: 'Invalid API key.'})
+			res.send(403)
+		}
+
+	});
+}
+
+GateKeeper.prototype.auth = function(req, res, next, apikey) {
+	
+	this.emit('User:find', req, res, next, {api_key: apikey});
+
+	// var data = { 
+	// 	user: {
+	// 		id: 1,
+	// 		name: 'Alex'
+	// 	} };
+
+	// this.emit('gatekeeper:auth:res', req, res, next, data)
+}
+
+module.exports = exports = GateKeeper;
